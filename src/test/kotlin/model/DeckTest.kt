6c27b88970f34c121c953634a3eb9adf65f70b12
middlefgr <2giwon@gmail.com>
2020-12-12 22:09:14 +0900

    [Add] - 테스크 코드 추가

--- src/test/kotlin/model/DeckTest.kt
+++ src/test/kotlin/model/DeckTest.kt
@@ -1,15 +1,15 @@
 package model
 
-import constant.Constant
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+
 
 class DeckTest {
 
     @Test
-    fun `덱 카드를 생성한다`() {
-        val deck = Deck().createDeck()
-        assertThat(deck.size).isEqualTo(Constant.MAX_CARDS)
+    fun `카드를 한장 뽑는다`() {
+        val deck = Deck()
+        deck.createDeck()
+        assertThat(deck.popCard()).isNotEqualTo(null)
     }
-
 }
---
+++ src/test/kotlin/model/GameTest.kt
@@ -0,0 +1,17 @@
+package model
+
+import constant.Constant
+
+class Game(val players: List<Player>, private val deck: Deck) {
+
+    fun addPlayerCard(index: Int) {
+        players[index].addCard(deck.popCard())
+    }
+
+    fun checkMoreCard(index: Int): Boolean {
+        return players[index].getCardRankSum(players[index].getCards()) <= Constant.WINNER_VALUE
+    }
+
+}
+
+class GameTest
