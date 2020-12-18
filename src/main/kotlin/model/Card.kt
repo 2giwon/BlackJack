a00b6a46040ad212368f9ef5db7e18eb2f42dc8b
2giwon <2giwon@gmail.com>
2020-12-18 21:34:02 +0900

    [FIX] - Rank, Suit 분리

--- src/main/kotlin/model/Card.kt
+++ src/main/kotlin/model/Card.kt
@@ -6,29 +6,6 @@ data class Card(
     val suit: Suit,
     val rank: Rank
 ) {
-    enum class Suit(val title: String) {
-        HEART("하트"),
-        DIAMOND("다이아몬드"),
-        SPADE("스페이드"),
-        CLUB("클로버")
-    }
-
-    enum class Rank(val `val`: String) {
-        ACE("A"),
-        TWO("2"),
-        THREE("3"),
-        FOUR("4"),
-        FIVE("5"),
-        SIX("6"),
-        SEVEN("7"),
-        EIGHT("8"),
-        NINE("9"),
-        TEN("10"),
-        JACK("J"),
-        QUEEN("Q"),
-        KING("K")
-    }
-
     companion object {
         fun isAce(rank: Rank): Boolean = Rank.ACE == rank
 
