8f984706a91cf26bb23ab48e818836e16fa0a588
middlefgr <2giwon@gmail.com>
2020-12-19 23:24:43 +0900

    [ADD] - 게임 카드 랭크 점수 합계를 계산하는 클래스 추가

--- src/main/kotlin/model/RankCalculator.kt
+++ src/main/kotlin/model/RankCalculator.kt
@@ -1,2 +1,21 @@
 package model
 
+import constant.Constant
+
+object RankCalculator {
+
+    fun getPlayerTotalCardPoints(player: Player): Int {
+        return player.playerCards
+            .asSequence()
+            .map { Card.getRankValue(it.rank) }
+            .sortedDescending()
+            .reduce { acc, value: Int ->
+                acc + if (value == Constant.ACE_VALUE) getAceCardValue(value) else value
+            }
+    }
+
+    private fun getAceCardValue(sum: Int): Int {
+        return if (sum + Constant.ACE_MAX_RANK > Constant.WINNER_VALUE) Constant.ACE_VALUE
+        else Constant.ACE_MAX_RANK
+    }
+}
--- src/test/kotlin/model/RankCalculatorTest.kt
+++ src/test/kotlin/model/RankCalculatorTest.kt
@@ -4,44 +4,14 @@ import constant.Constant
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-object RankCalculator {
-
-    fun getPlayerTotalCardPoints(player: Player): Int {
-        return player.getCards()
-            .asSequence()
-            .map { Card.getRankValue(it.rank) }
-            .sortedDescending()
-            .reduce { acc, value: Int ->
-                acc + if (value == Constant.ACE_VALUE) getAceCardValue(value) else value
-            }
-    }
-
-    fun getCardRankSum(cards: List<Card>): Int {
-        var sum = 0
-        cards.forEach {
-            sum += if (Card.isAce(it.rank)) getAceCardValue(sum) else Card.getRankValue(it.rank)
-        }
-
-        return sum
-    }
-
-    private fun getAceCardValue(sum: Int): Int {
-        // ACE는 1 또는 11
-        return if (sum + Constant.ACE_MAX_RANK > Constant.WINNER_VALUE) Constant.ACE_VALUE
-        else Constant.ACE_MAX_RANK
-    }
-}
-
 class RankCalculatorTest {
-
 
     @Test
     fun `플레이어가 Ace를 들고 있을때 카드합계가 21이 되는지 확인한다`() {
-        val player = Player(
-            listOf(
-
-            ), "a"
-        )
+        val player = Player("a")
+        player.addCard(Card(Suit.DIAMOND, Rank.FOUR))
+        player.addCard(Card(Suit.HEART, Rank.SIX))
+        player.addCard(Card(Suit.CLUB, Rank.ACE))
 
         assertThat(RankCalculator.getPlayerTotalCardPoints(player)).isEqualTo(Constant.WINNER_VALUE)
     }
